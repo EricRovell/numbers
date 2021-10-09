80be114e167390c5132d286192bf47bef0066d0c
Eric Rovell <ericrovell@gmail.com>
2021-10-10 01:02:22 +0300

    feat(component): new `Link` component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let href: string;
+	export let targetBlank = false;
+	export let nofollow = false;
+
+	$: target = targetBlank ? "_blank" : "";
+	$: external = href.indexOf("://") !== -1;
+
+	$: rel = `${external ? "noopener noreferrer" : ""}` + `${nofollow ? "nofollow" : ""}`;
+</script>
+
+<a {href} {target} {rel} {...$$restProps}>
+	<slot />
+</a>
+
+<style>
+	a {
+		color: var(--color-accent, inherit);
+	}
+</style>
