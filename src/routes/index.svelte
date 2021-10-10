89b906c4bc036df11a46a018a4c54e1ef63686b7
Eric Rovell <ericrovell@gmail.com>
2021-10-10 14:50:12 +0300

    feat(route): create `index` route

---
+++ src/routes/index.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { Numbers, Form, InputNumber, Button } from "@components";
+
+	let radixes = [ 10, 2 ];
+
+	function addNumber(event): void {
+		const data = new FormData(event.target);
+		const radix = Number(data.get("radix"));
+		radixes = [ radix, ...radixes ].slice(0, 5);
+	}
+</script>
+
+<main>
+	<section label="controls">
+		<Form on:submit={addNumber}>
+			<div class="form">
+				<InputNumber
+					min={2}
+					max={36}
+					name="radix"
+					required
+					placeholder="Add another radix based number"
+				/>
+				<Button shape="square" variant="accent" appearance="text">
+					+
+				</Button>
+			</div>
+		</Form>
+	</section>
+	<section>
+		<Numbers
+			ranks={[ 0, 0, 0, 0 ]}
+			{radixes}
+		/>
+	</section>
+</main>
+
+<style>
+	main {
+		display: grid;
+		gap: var(--space-xl);
+		align-content: start;
+		justify-items: center;
+
+		min-height: calc(100vh - 50px);
+		margin: var(--space-xl) 0;
+	}
+
+	section[label="controls"] {
+		width: min(75%, 375px);
+	}
+
+	.form {
+		display: flex;
+		align-items: center;
+		padding: var(--space-s);
+		gap: var(--space-xs);
+	}
+</style>
