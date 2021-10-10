ab6bdd124e8952518326d39ff96679bad5559116
Eric Rovell <ericrovell@gmail.com>
2021-10-10 12:51:03 +0300

    feat(component): new `Form` wrapper component

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,17 @@
+<form on:submit|preventDefault>
+	<slot />
+</form>
+
+<style>
+	form {
+		--outline-color: unset;
+
+		border: var(--border-1);
+		outline: 1px solid var(--outline-color);
+		transition: outline-color 0.5s linear;
+	}
+
+	form:focus-within {
+		--outline-color: var(--color-accent);
+	}
+</style>
