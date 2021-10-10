7663bb314bd492df81321ca52f2b274676beca34
Eric Rovell <ericrovell@gmail.com>
2021-10-10 14:45:29 +0300

    feat: components index

---
+++ src/lib/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button.svelte";
+export { default as Form } from "./Form.svelte";
+export { default as InputNumber } from "./InputNumber.svelte";
+export { default as Link } from "./Link.svelte";
+export { default as Numbers } from "./number/Numbers.svelte";
