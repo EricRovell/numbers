c5e1520437873c774e1a3cf9ce024a5957ece992
Eric Rovell <ericrovell@gmail.com>
2021-10-18 02:58:22 +0300

    feat(component): Menu component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,3 +3,4 @@ export { default as Form } from "./Form.svelte";
 export { default as InputNumber } from "./InputNumber.svelte";
 export { default as Link } from "./Link.svelte";
 export { default as Numbers } from "./number/Numbers.svelte";
+export * as Navigation from "./menu";
