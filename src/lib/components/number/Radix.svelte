7d893efc7b763899923af956ca8ba7503a7d75a8
Eric Rovell <ericrovell@gmail.com>
2021-10-17 15:50:57 +0300

    refactor(styles): compose styles from module

--- src/lib/components/number/Radix.svelte
+++ src/lib/components/number/Radix.svelte
@@ -1,24 +1,9 @@
 <script lang="ts">
+	import styles from "./style.module.css";
+
 	export let radix: number;
 </script>
 
-<div>
+<div class={styles.radix}>
 	<span>{radix}</span>
 </div>
-
-<style>
-	div {
-		position: sticky;
-		right: 0;
-
-		display: flex;
-		place-items: center;
-		padding: var(--space-m);
-		background-color: var(--color-surface-300);
-		border: var(--border-1);
-		border-radius: 5px;
-
-		font-size: var(--font-size-5);
-		color: var(--color-gray-800);		
-	}
-</style>
