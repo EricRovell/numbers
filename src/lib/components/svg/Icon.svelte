57ae21148fe8bdf2b6376b5f53f7928227197534
Eric Rovell <ericrovell@gmail.com>
2021-10-19 12:27:01 +0300

    feat(component): `SVG` rendering components

---
+++ src/lib/components/svg/Icon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import SVG from "./SVG.svelte";
+
+	export let size = "1em";
+	export let fill = "currentColor";
+</script>
+
+<SVG
+	{...$$props}
+	height={size}
+	width={size}
+	{fill}
+	{...$$restProps}
+>
+	<slot />
+</SVG>
---
+++ src/lib/components/svg/SVG.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	export let title: string | undefined =  undefined;
+	export let viewBox = "0 0 100 100";
+	export let svg: string | undefined = undefined;
+	export let path: string | undefined = undefined;
+	export let paths: string[] | undefined = undefined;
+	export let width: string | undefined  = undefined;
+	export let height: string | undefined  = undefined;
+	export let stroke: string | undefined = undefined;
+	export let fill: string | undefined = undefined;
+</script>
+
+<svelte:options namespace="svg" />
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  {width}
+  {height}
+  {viewBox}
+	{stroke}
+	{fill}
+  aria-labelledby={title}
+	{...$$restProps}>
+		{#if title}
+			<title>{title}</title>
+		{/if}
+    {#if svg}
+      <g>
+        {@html svg}
+      </g>
+    {/if}
+		{#if path}
+			<path d={path} />
+		{/if}
+		{#if paths}
+			{#each paths as path}
+				<path d={path} />
+			{/each}
+		{/if}
+		<g>
+			<slot />
+		</g>
+</svg>
