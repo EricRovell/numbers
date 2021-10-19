c8bf8031f5b4c25b72cd74f92ddbab6b8fcf1555
Eric Rovell <ericrovell@gmail.com>
2021-10-19 14:49:35 +0300

    feat(component): `Datalist` component

---
+++ src/lib/components/Datalist.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+  interface DatalistItem {
+    label?: string;
+    value: string | number;
+  }
+</script>
+
+<script lang="ts">
+  export let id: string | undefined = undefined;
+  export let options: Array<DatalistItem> = [];
+</script>
+
+{#if id && options.length}
+	<datalist {id}>
+		{#each options as { label, value }}
+			<option {value}>{label}</option>
+		{/each}
+	</datalist>
+{/if}
