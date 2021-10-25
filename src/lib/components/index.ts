ad680b49da7b5697cbe3b2a180d675958783491d
Eric Rovell <ericrovell@gmail.com>
2021-10-25 13:47:54 +0300

    feat(component): moved numbers app logic into widget

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,7 +2,7 @@ export { default as Button } from "./Button.svelte";
 export { default as Form } from "./Form.svelte";
 export { default as InputNumber } from "./InputNumber.svelte";
 export { default as Link } from "./Link.svelte";
-export { default as Numbers } from "./number/Numbers.svelte";
+export { default as Numbers } from "./number/App.svelte";
 export { default as FormRadix } from "./number/FormRadix.svelte";
 export * as Navigation from "./menu";
 export { default as Datalist } from "./Datalist.svelte";
